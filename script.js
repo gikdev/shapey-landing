f495b1536316df3eb50b97e9c199b65056b10e74
Mohammad Mahdi Bahrami <90021059+gikdev@users.noreply.github.com>
2024-04-10 17:36:23 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      inter: `"Inter", sans-serif`,
+      merriweather: `"Merriweather", serif`,
+    },
+    extend: {
+      colors: {
+        primary: '#4D73F8',
+      },
+    }
+  }
+}
